bef8adc5ae486acd298803e158ee769bfc504104
Jorge <103543373+AustralCoder@users.noreply.github.com>
2026-08-01 22:29:35 -0300

    feat: create initial requires and async function

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,13 @@
+const {PrismaClient} = require('@prisma/client');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const prisma = new PrismaClient();
+
+
+exports.register = async(req, res) => {
+    try{
+
+    } catch (error) {
+
+    }
+}
