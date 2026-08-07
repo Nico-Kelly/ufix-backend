49da5dc0f7089785657c69bd117a0741052ffa19
Jorge <103543373+AustralCoder@users.noreply.github.com>
2026-08-07 20:07:21 -0300

    feat: add decode logic to the try catch

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -7,5 +7,18 @@ exports.authenticate = (req, res, next) => {
         return res.status(401).json({error: 'Token no proporcionado'});
     }
 
+    const token = authHeader.split('')[1];
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+        req.user = decoded; // {userId, Role}
+
+        next();
+
+    } catch (error) {
+        return res.status(401).json({error: 'Token inválido o expirado'});
+    };
+
     
 }
